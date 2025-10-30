80b463b2673c0ed3f9ad06c8b919c86089348524
Samantha Batson <rosemariesamantharose@gmail.com>
2025-10-30 10:21:04 -0700

    maybe final

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,9 +43,16 @@ dependencies {
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
+    implementation(libs.core)
+    implementation(libs.androidx.junit)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+
+    androidTestImplementation("androidx.test:core:1.5.0")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.arch.core:core-testing:2.2.0")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 
     implementation(libs.room.runtime)
     annotationProcessor( libs.room.compiler)
